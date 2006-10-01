392d5d9bf258046d9034aca9fec5174cfc507a37
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2006-10-01 15:03:54 +0000

    *** empty log message ***
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk@15 35eed6a1-492f-0410-aaa5-912e50059252

--- jupload2/src/wjhk/jupload2/JUploadApplet.java
+++ jupload2/src/wjhk/jupload2/JUploadApplet.java
@@ -30,8 +30,8 @@ public class JUploadApplet extends Applet{
       "Java Applet wrapper for JUploadPanel.";
   public static final String AUTHOR = "William JinHua Kwong (updated by Etienne Gauthier)";
 
-  public static final String VERSION = "2.1.2.4";
-  public static final String LAST_MODIFIED = "18 sept 2006";
+  public static final String VERSION = "2.1.3.0";
+  public static final String LAST_MODIFIED = "01 oct 2006";
 
   //----------------------------------------------------------------------
 
