b623079278ee28730df3f78b6c204441fe057370
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2007-02-03 13:57:05 +0000

    2.7.0rc3 (Corrected a major bug: uploading several files one by one would not work since 2.7.0rc1)
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk/jupload2@62 35eed6a1-492f-0410-aaa5-912e50059252

--- src/wjhk/jupload2/JUploadApplet.java
+++ src/wjhk/jupload2/JUploadApplet.java
@@ -33,8 +33,8 @@ public class JUploadApplet extends Applet{
       "Java Applet wrapper for JUploadPanel.";
   public static final String AUTHOR = "William JinHua Kwong (updated by Etienne Gauthier)";
 
-  public static final String VERSION = "2.7.0rc2";
-  public static final String LAST_MODIFIED = "27 jan 2007";
+  public static final String VERSION = "2.7.0rc3";
+  public static final String LAST_MODIFIED = "02 feb 2007";
 
   //----------------------------------------------------------------------
 
