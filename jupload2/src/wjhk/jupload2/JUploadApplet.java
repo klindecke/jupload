24581a88decb449ea8de455dbb31e9db8ed2acf5
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2007-05-23 16:35:34 +0000

    Switch to Version 3.0.0
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk@165 35eed6a1-492f-0410-aaa5-912e50059252

--- jupload2/src/wjhk/jupload2/JUploadApplet.java
+++ jupload2/src/wjhk/jupload2/JUploadApplet.java
@@ -55,7 +55,7 @@ public class JUploadApplet extends Applet {
     /**
      * The version of this applet.
      */
-    public final static String VERSION = "3.0.0a1 ($Revision$)";
+    public final static String VERSION = "3.0.0a3 ($Revision$)";
 
     /**
      * The last modification of this applet.
