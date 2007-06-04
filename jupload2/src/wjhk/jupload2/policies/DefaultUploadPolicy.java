0b17a32455cc4020e2e74205e476357ef39008c8
felfert <felfert@35eed6a1-492f-0410-aaa5-912e50059252>
2007-06-04 19:56:27 +0000

    - Organized imports
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk@238 35eed6a1-492f-0410-aaa5-912e50059252

--- jupload2/src/wjhk/jupload2/policies/DefaultUploadPolicy.java
+++ jupload2/src/wjhk/jupload2/policies/DefaultUploadPolicy.java
@@ -35,7 +35,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.net.ConnectException;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
