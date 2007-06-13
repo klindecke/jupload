811f1b3caf6ccad73134f26e48501209d6affd5c
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2007-06-13 10:38:33 +0000

    Enhancement of the PHP demo script
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk/jupload2@274 35eed6a1-492f-0410-aaa5-912e50059252

--- wwwroot/upload_dummy.php
+++ wwwroot/upload_dummy.php
@@ -1,5 +1,20 @@
 <?php
-foreach ($_FILES as $val) {
-    unlink($val['tmp_file']);
+$bSuccess=true;
+foreach ($_FILES as $file => $fileArray) {
+	echo("File key: $file\n");
+	foreach ($fileArray as $item=>$val) {
+		echo("  Data received: $item=>$val\n");
+	}
+	//Let's manipulate the received file: in this demo, we just want to remove it!
+	unlink($fileArray['tmp_name']);
+	echo("  Temp file ${fileArray['tmp_name']} removed !\n");
 }
-echo "SUCCESS\n";
+echo("\n");
+//Let's say to the applet that it's a success or a failure:
+if ($bSuccess) {
+	echo "SUCCESS\n";
+} else {
+	$msgError = "Unknown error";
+	echo "ERROR: $msgError\n";
+}
+echo "End of upload_dummy.php script\n";
