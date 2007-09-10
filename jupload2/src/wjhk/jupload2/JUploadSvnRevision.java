4014ac52e0ca0622249f152b8afa6c095b3d3b1b
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2007-09-10 10:30:39 +0000

    Fake commit, to allow compilation from eclipse: the JUploadSvnRevision.
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk@320 35eed6a1-492f-0410-aaa5-912e50059252

---
+++ jupload2/src/wjhk/jupload2/JUploadSvnRevision.java
@@ -0,0 +1,18 @@
+
+			// WARNING - This file is auto-generated
+			// ANY CHANGES WILL BE OVERWRITTEN BY THE BUILD-PROCESS!
+
+
+// This commit is a 'fake' commit: this file is needed by the eclipse build
+// The file is overwritted, and mark as ignored by the build.xml ant build.
+
+			package wjhk.jupload2;
+			class JUploadSvnRevision {
+				final static String REVISION = "Unknown";
+				//LASTCHANGED is the compilation date (since 3.1)
+				final static String LASTCHANGED = "Unknown";
+				//Last commit of the src directory: may be much older
+				//than the last commit of files contained in the src dir!
+				final static String LASTCHANGED_src = "Unknown";
+			}
+		
