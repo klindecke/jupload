51e43662125ccf2183f556d96f6c9d2aeaed6995
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2008-05-07 20:58:37 +0000

    3.4.0 release
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk/jupload2@465 35eed6a1-492f-0410-aaa5-912e50059252

--- src/wjhk/jupload2/JUploadApplet.java
+++ src/wjhk/jupload2/JUploadApplet.java
@@ -78,7 +78,7 @@ public class JUploadApplet extends Applet {
      * JUploadApplet.java file. The revision is added at build time, by the
      * build.xml ant file, packaged with the applet.
      */
-    public final static String VERSION = "3.3.2rc3 [SVN-Rev: "
+    public final static String VERSION = "3.4.0 [SVN-Rev: "
             + svnProperties.getProperty("revision") + "]";
 
     /**
