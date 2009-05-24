1f1bf28161f94b6dfbbca35c4041fbe36d230357
etienne_sf <etienne_sf@35eed6a1-492f-0410-aaa5-912e50059252>
2009-05-24 19:24:58 +0000

    JUploadDaemon creation
    git-svn-id: https://jupload.svn.sourceforge.net/svnroot/jupload/trunk@771 35eed6a1-492f-0410-aaa5-912e50059252

---
+++ jupload2/src/wjhk/jupload2/JUploadDaemon.java
@@ -0,0 +1,94 @@
+package wjhk.jupload2;
+
+import javax.swing.JFrame;
+
+import wjhk.jupload2.context.JUploadContext;
+import wjhk.jupload2.context.JUploadContextExecutable;
+import wjhk.jupload2.policies.UploadPolicy;
+
+// FIXME Complete this class (when daemon process is defined)
+
+/**
+ * 
+ * This class allows to use JUpload as a stand alone application. It can then be
+ * used to manage upload, as do the applet. Or it can be used as a daemon. In
+ * this case the applet 'paste' files to the daemon, and the daemon is
+ * responsible for the upload. This is a good point when uploading big files:
+ * the user can go on browsing, or close his/her browser. The daemon will keep
+ * on uploading the file(s). <BR>
+ * The configuration can be stored in the jar file, or in a property file
+ * available on the net, through a URL. See the {link {@link #main(String[])}
+ * method for details.<BR>
+ * The daemon parameters are the same for the applet and the daemon. They are
+ * described on the {@link UploadPolicy} page.
+ * 
+ * @author etienne_sf
+ * 
+ */
+public class JUploadDaemon extends JFrame {
+
+    /** A generated serialVersionUID */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The URL, that 'perhaps' was given to the main method. Used only to
+     * transfer the value between the invokeLater and the actual execution of
+     * this method. It's not very clean, but I guess there will never be two
+     * execution of the daemon that will start in the same quarter of second
+     * with a different URL.
+     */
+    private static String propertiesURL = null;
+
+    /**
+     * The current execution context.
+     */
+    JUploadContext juploadContext = null;
+
+    /**
+     * Default constructor.
+     */
+    public JUploadDaemon() {
+        // TODO add a way to personalize the window title.
+        super("JUpload daemon");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Create the GUI and show it. For thread safety, this method should be
+     * invoked from the event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        JUploadDaemon juploadDaemon = new JUploadDaemon();
+        juploadDaemon.juploadContext = new JUploadContextExecutable(
+                juploadDaemon, propertiesURL);
+
+        // Display the window.
+        juploadDaemon.pack();
+        juploadDaemon.setVisible(true);
+    }
+
+    /**
+     * The start of the application, when launched as a Stand Alone one. If an
+     * argument is given, it must be a valid URL to the JUpload configuration
+     * file. The daemon will load it as a property file. The allowed parameters
+     * and values are the same as the applet parameters. These are indicated in
+     * the {@link wjhk.jupload2.policies.UploadPolicy} page.<BR>
+     * If this URL is not given, the /conf folder in the jar file must contain
+     * the daemon property file.
+     * 
+     * @param args args[1] is optional, and may contain the URL pointing to the
+     *            configuration page.
+     */
+    public static void main(String[] args) {
+        propertiesURL = null;
+        if (args.length > 0) {
+            propertiesURL = args[0];
+        }
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
---
+++ jupload2/src/wjhk/jupload2/context/JUploadContextExecutable.java
@@ -0,0 +1,278 @@
+//
+// $Id: JUploadApplet.java 750 2009-05-06 14:36:50Z etienne_sf $
+//
+// jupload - A file upload applet.
+// Copyright 2007 The JUpload Team
+//
+// Created: ?
+// Creator: William JinHua Kwong
+// Last modified: $Date: 2009-05-06 16:36:50 +0200 (mer., 06 mai 2009) $
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version. This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details. You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation, Inc.,
+// 675 Mass Ave, Cambridge, MA 02139, USA.
+
+package wjhk.jupload2.context;
+
+import java.awt.Cursor;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Properties;
+import java.util.Vector;
+
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import wjhk.jupload2.JUploadDaemon;
+import wjhk.jupload2.exception.JUploadException;
+
+/**
+ * Implementation of the {@link JUploadContext}, for an executable, that is: for
+ * a stand alone application. One such context is created at run time. Its main
+ * capabilities, is to load the properties either by a file in the jar file (see
+ * DAEMON_PROPERTIES_FILE), or an URL given to the
+ * {@link JUploadDaemon#main(String[])} method.
+ * 
+ * @see DefaultJUploadContext
+ * @see JUploadDaemon
+ * 
+ * @author etienne_sf
+ * @version $Revision: 750 $
+ */
+public class JUploadContextExecutable extends DefaultJUploadContext {
+
+    final static String DEFAULT_PROPERTIES_FILE = "/conf/default_daemon.properties";
+
+    final static String DAEMON_PROPERTIES_FILE = "/conf/daemon.properties";
+
+    /**
+     * The main window of the application.
+     */
+    private JFrame jframe = null;
+
+    /**
+     * The content pane of this window.
+     */
+    JPanel contentPane = null;
+
+    /**
+     * Content of the /conf/default_deamon.properties file. These value override
+     * default value, that would be wrong values for the daemon standalone
+     * application.
+     */
+    Properties defaultProperties = null;
+
+    /**
+     * Content of the /conf/_deamon.properties file. These value are the
+     * properties given to parameterize the daemon, according to the specific
+     * needs of the project.
+     */
+    Properties daemonProperties = null;
+
+    /**
+     * The constructor of the context, which needs the top level container to be
+     * created.
+     * 
+     * @param jframe The owner TopLevelWindow
+     * @param propertiesURL The URL where the configuration properties for the
+     *            daemon can be read. If null, the daemon try to read the
+     *            /conf/daemon.properties file, in the current jar.
+     */
+    public JUploadContextExecutable(JFrame jframe, String propertiesURL) {
+        if (jframe == null) {
+            throw new IllegalArgumentException("theApplet may not be null");
+        }
+        this.jframe = jframe;
+
+        // Load default properties
+        defaultProperties = loadPropertiesFromFile(DEFAULT_PROPERTIES_FILE,
+                null);
+
+        // Load daemon properties: from the given URL or from the file.
+        if (propertiesURL == null) {
+            // No URL given. We load properties from the 'standard' file, in the
+            // jar.
+            daemonProperties = loadPropertiesFromFile(DAEMON_PROPERTIES_FILE,
+                    defaultProperties);
+        } else {
+            // Let's load the properties from this URL.
+            daemonProperties = loadPropertiesFromURL(propertiesURL,
+                    defaultProperties);
+        }
+
+        // Now, we're ready. Let's initialize the DefaultJUploadContext.
+        init(this.jframe);
+    }
+
+    /**
+     * Creates and loads a property file, and return the loaded result.
+     * 
+     * @param filename The name of the file, which contains the properties to
+     *            load
+     * @param defaultProperties The default properties value. Put null if no
+     *            default Properties should be used.
+     * @return The loaded properties. It's empty if an error occurs.
+     */
+    Properties loadPropertiesFromFile(String filename,
+            Properties defaultProperties) {
+        Properties properties = new Properties(defaultProperties);
+        try {
+            // TODO use this.getClass() ?
+            properties.load(Class.forName("wjhk.jupload2.JUploadApplet")
+                    .getResourceAsStream(filename));
+        } catch (IOException e1) {
+            System.out.println("Error while loading " + filename + " ("
+                    + e1.getClass().getName() + ")");
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            System.out.println("Error while loading " + filename + " ("
+                    + e1.getClass().getName() + ")");
+            e1.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    /**
+     * Creates and loads a property file, and return the loaded result.
+     * 
+     * @param propertiesURL The url that points to the properties configuration
+     *            file for the daemon.
+     * @param defaultProperties The default properties value. Put null if no
+     *            default Properties should be used.
+     * @return The loaded properties. It's empty if an error occurs.
+     */
+    private Properties loadPropertiesFromURL(String propertiesURL,
+            Properties defaultProperties) {
+        Properties properties = new Properties(defaultProperties);
+        URL url;
+        try {
+            url = new URL(propertiesURL);
+            URLConnection urlConnection = url.openConnection();
+            properties.load(urlConnection.getInputStream());
+        } catch (MalformedURLException e) {
+            System.out.println("Error while loading url " + propertiesURL
+                    + " (" + e.getClass().getName() + ")");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error while loading url " + propertiesURL
+                    + " (" + e.getClass().getName() + ")");
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    /**
+     * Get a String parameter value from applet properties or System properties.
+     * 
+     * @param key The name of the parameter to fetch.
+     * @param def A default value which is used, when the specified parameter is
+     *            not set.
+     * @return The value of the applet parameter (resp. system property). If the
+     *         parameter was not specified or no such system property exists,
+     *         returns the given default value.
+     */
+    public String getParameter(String key, String def) {
+        String paramStr = (daemonProperties.getProperty(key) != null ? daemonProperties
+                .getProperty(key)
+                : def);
+        displayDebugParameterValue(key, paramStr);
+        return paramStr;
+    }
+
+    /** {@inheritDoc} */
+
+    public int getParameter(String key, int def) {
+        String paramDef = Integer.toString(def);
+        String paramStr = daemonProperties.getProperty(key) != null ? daemonProperties
+                .getProperty(key)
+                : paramDef;
+        displayDebugParameterValue(key, paramStr);
+        return parseInt(paramStr, def);
+    }
+
+    /** {@inheritDoc} */
+    public float getParameter(String key, float def) {
+        String paramDef = Float.toString(def);
+        String paramStr = daemonProperties.getProperty(key) != null ? daemonProperties
+                .getProperty(key)
+                : paramDef;
+        displayDebugParameterValue(key, paramStr);
+        return parseFloat(paramStr, def);
+    }
+
+    /** {@inheritDoc} */
+    public long getParameter(String key, long def) {
+        String paramDef = Long.toString(def);
+        String paramStr = daemonProperties.getProperty(key) != null ? daemonProperties
+                .getProperty(key)
+                : paramDef;
+        displayDebugParameterValue(key, paramStr);
+        return parseLong(paramStr, def);
+    }
+
+    /** {@inheritDoc} */
+    public boolean getParameter(String key, boolean def) {
+        String paramDef = (def ? "true" : "false");
+        String paramStr = daemonProperties.getProperty(key) != null ? daemonProperties
+                .getProperty(key)
+                : paramDef;
+        displayDebugParameterValue(key, paramStr);
+        return parseBoolean(paramStr, def);
+    }// getParameter(boolean)
+
+    /** {@inheritDoc} */
+    public void displayURL(String url, boolean success) {
+        throw new UnsupportedOperationException(
+                "JUploadContextExecution.displayURL(): Not implemented yet!");
+    }
+
+    /** {@inheritDoc} */
+    public JApplet getApplet() {
+        throw new UnsupportedOperationException(
+                "Can't use getApplet(), when using the JUploadDaemon!");
+    }
+
+    /** {@inheritDoc} */
+    public Cursor getCursor() {
+        return this.jframe.getCursor();
+    }
+
+    /**
+     * This class doesn't control the URL. It expects it to be already
+     * normalized. No work here. {@inheritDoc}
+     * */
+    public String normalizeURL(String url) throws JUploadException {
+        return url;
+    }
+
+    /** {@inheritDoc} */
+    public void readCookieAndUserAgentFromNavigator(Vector<String> headers) {
+        throw new UnsupportedOperationException(
+                "Can't use readCookieAndUserAgentFromNavigator(), when using the JUploadDaemon!");
+    }
+
+    /** {@inheritDoc} */
+    public Cursor setCursor(Cursor cursor) {
+        Cursor previousCursor = this.jframe.getCursor();
+        this.jframe.setCursor(cursor);
+        return previousCursor;
+    }
+
+    /** {@inheritDoc} */
+    public void showStatus(String status) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
